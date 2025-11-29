563e951cbaa362763e43b55403e9d95a69c79001
tom106gunma <tom106.net@gmail.com>
2025-11-30 06:29:00 +0900

    ボタンのクリック時に stateを更新

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,22 @@ export const App = () => {
   const [dogUrl, setDogUrl] = useState(
     'https://images.dog.ceo/breeds/retriever-flatcoated/n02099267_2259.jpg',
   )
+
+  const handleClick = newUrl => () => {
+    setDogUrl(newUrl)
+  }
+
   return (
     <div>
       <header>Dogアプリ</header>
       <img src={dogUrl} alt="dog" />
+      <button
+        onClick={handleClick(
+          'https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg',
+        )}
+      >
+        更新
+      </button>
     </div>
   )
 }
